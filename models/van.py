f38c9c68b59dd612fbd019047495ad77e16647a2
MenghaoGuo <38318671+MenghaoGuo@users.noreply.github.com>
2022-02-20 22:07:46 +0800

    Update van.py

--- models/van.py
+++ models/van.py
@@ -84,9 +84,6 @@ class SpatialAttention(nn.Module):
 
 
 class Block(nn.Module):
-    # def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                #  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, sr_ratio=1):
-
     def __init__(self, dim, mlp_ratio=4., drop=0.,drop_path=0., act_layer=nn.GELU):
         super().__init__()
         self.norm1 = nn.BatchNorm2d(dim)
