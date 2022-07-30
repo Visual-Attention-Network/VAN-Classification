e19779b53a1b0828b51ecb4412d577541aee83a7
MenghaoGuo <38318671+MenghaoGuo@users.noreply.github.com>
2022-07-30 12:38:48 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-MODEL=van_tiny # van_{tiny, small, base, large}
-DROP_PATH=0.1 # drop path rates [0.1, 0.1, 0.1, 0.2] for [tiny, small, base, large]
+MODEL=van_b0 # van_{b0, b1, b2, b3}
+DROP_PATH=0.1 # drop path rates [0.1, 0.1, 0.1, 0.2] for [b0, b1, b2, b3]
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash distributed_train.sh 8 /path/to/imagenet \
 	  --model $MODEL -b 128 --lr 1e-3 --drop-path $DROP_PATH
